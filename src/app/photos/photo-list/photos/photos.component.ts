88a7e9f42653bf002853cdf556e37190d8591524
Matheus Viana <matheus_o_viana@hotmail.com>
2019-12-13 14:45:08 -0300

    indentation and changed selector

--- src/app/photos/photo-list/photos/photos.component.ts
+++ src/app/photos/photo-list/photos/photos.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-photos',
+  selector: 'ap-photos',
   templateUrl: './photos.component.html',
   styleUrls: ['./photos.component.scss']
 })
 export class PhotosComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
