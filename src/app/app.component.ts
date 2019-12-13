990dc51a441feafc07cc3f71aafd9a913c0f980c
Matheus Viana <matheus_o_viana@hotmail.com>
2019-12-12 23:47:25 -0300

    delete: deleted data from app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  photos = [
-    {
-      url:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg',
-      description: 'Lion'
-    },
-    {
-      url:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Lioness_Etosha_NP.jpg/500px-Lioness_Etosha_NP.jpg',
-      description: 'Lioness'
-    }
-  ];
+  photos = [];
 }
