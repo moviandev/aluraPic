1102ce79ebc6f753988cc8a1fc022513f2483921
Matheus Viana <matheus_o_viana@hotmail.com>
2019-12-13 11:30:35 -0300

    feats: added ngOnInint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { PhotoService } from './photos/photo/photo.service';
 import { Photo } from './photos/photo/photo';
@@ -8,11 +8,15 @@ import { Photo } from './photos/photo/photo';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   photos: Photo[] = [];
 
-  constructor(photoService: PhotoService) {
-    photoService.listFromUser('flavio').subscribe(pht => {
+  // COnstructor just to inject data
+
+  constructor(private photoService: PhotoService) {}
+
+  ngOnInit(): void {
+    this.photoService.listFromUser('flavio').subscribe(pht => {
       this.photos = pht;
     });
   }
