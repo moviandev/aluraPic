babd97ad7d1a4edfcd940b54ccc97eb78b07b050
Matheus Viana <matheus_o_viana@hotmail.com>
2019-12-12 23:51:27 -0300

    added constructor to http client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'ap-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   photos = [];
+
+  constructor(http: HttpClient) {
+    console.log(http);
+  }
 }
