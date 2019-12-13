140a20fcc1c972deb2bcffd77472c1cfe1f0ab32
Matheus Viana <matheus_o_viana@hotmail.com>
2019-12-12 23:59:03 -0300

    chore: added tslint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
+  // tslint:disable-next-line: component-selector
   selector: 'ap-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
