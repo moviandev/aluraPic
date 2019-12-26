81c5934f323cf15d40d2409c53134f31fea5b5c6
Matheus Viana <matheus_o_viana@hotmail.com>
2019-12-26 14:50:21 -0300

    chore: added params service

--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Photo } from './photo';
 import { Observable } from 'rxjs';
+
+import { Photo } from './photo';
 
 const API = 'http://localhost:3000';
 
@@ -10,6 +11,11 @@ export class PhotoService {
   constructor(private http: HttpClient) {}
 
   listFromUser(userName: string): Observable<Photo[]> {
+    return this.http.get<Photo[]>(`${API}/${userName}/photos`);
+  }
+
+  listFromUserPaginated(userName: string, page: number): Observable<Photo[]> {
+    const params = new HttpParams();
     return this.http.get<Photo[]>(`${API}/${userName}/photos`);
   }
 }
