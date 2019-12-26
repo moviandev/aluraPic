a934d5673e00d193b7456379e6f78ad7093d3f08
Matheus Viana <matheus_o_viana@hotmail.com>
2019-12-26 15:09:23 -0300

    chore: fixed load button selector and added input

--- src/app/photos/photo-list/load-button/load-button.component.ts
+++ src/app/photos/photo-list/load-button/load-button.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-load-button',
+  selector: 'ap-load-button',
   templateUrl: './load-button.component.html',
   styleUrls: ['./load-button.component.scss']
 })
 export class LoadButtonComponent implements OnInit {
-  hasMore = false;
+  @Input() hasMore = false;
 
   constructor() {}
 
