b1979ec394680275e4a0c1beba61a2b98e3e05c7
Matheus Viana <matheus_o_viana@hotmail.com>
2019-12-26 10:34:46 -0300

    chore: added filter property

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -10,6 +10,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class PhotoListComponent implements OnInit {
   photos: Photo[] = [];
+  filter: string;
 
   // Constructor just to inject data
   constructor(
