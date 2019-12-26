641adcc9f56aa461c789b0ac110b723f75c56ec4
Matheus Viana <matheus_o_viana@hotmail.com>
2019-12-26 15:17:19 -0300

    chore: added photoService

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -5,6 +5,7 @@ import { Subject } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
 
 import { Photo } from '../photo/photo';
+import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'ap-photo-list',
@@ -15,10 +16,13 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   photos: Photo[] = [];
   filter: string;
   debounce: Subject<string> = new Subject<string>();
-  hasMore: boolean = false;
+  hasMore = false;
 
   // Constructor just to inject data
-  constructor(private activatedRoute: ActivatedRoute) {}
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private photoService: PhotoService
+  ) {}
 
   ngOnInit(): void {
     this.photos = this.activatedRoute.snapshot.data.photos;
