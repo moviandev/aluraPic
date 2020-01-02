864df3d302c7dd860a2e023eb5e360d5ff394b73
Matheus Viana <matheus_o_viana@hotmail.com>
2020-01-02 14:53:26 -0300

    chore: code refactoring

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -40,7 +40,7 @@ export class PhotoListComponent implements OnInit, OnDestroy {
     this.photoService
       .listFromUserPaginated(this.userName, ++this.currentPage)
       .subscribe(p => {
-        this.photos = this.photos.concat(p);
+        this.photos.push(...p);
         if (!p.length) {
           this.hasMore = false;
         }
