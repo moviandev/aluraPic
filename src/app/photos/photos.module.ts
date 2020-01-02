e8684e156f0ced780e234665b65c642e2f14c13e
Matheus Viana <matheus_o_viana@hotmail.com>
2020-01-02 11:16:46 -0300

    chore: moved photos module to its own file

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 
-import { PhotoComponent } from './photos/photo/photo.component';
-import { PhotoListComponent } from './photos/photo-list/photo-list.component';
-import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
-import { PhotosComponent } from './photos/photo-list/photos/photos.component';
-import { FilterByDescriptionPipe } from './photos/photo-list/filterByDescription.pipe';
-import { LoadButtonComponent } from './photos/photo-list/load-button/load-button.component';
+import { PhotoComponent } from './photo/photo.component';
+import { PhotoListComponent } from './photo-list/photo-list.component';
+import { PhotoFormComponent } from './photo-form/photo-form.component';
+import { PhotosComponent } from './photo-list/photos/photos.component';
+import { FilterByDescriptionPipe } from './photo-list/filterByDescription.pipe';
+import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
 
 /*
   A module can have lots ad lots of declarions, but I have to export those declarions to be accessible outside of this module
