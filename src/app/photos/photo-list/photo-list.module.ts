3f3ae8f7272fe7bee4c5da8e60cd5377ba6a508d
Matheus Viana <matheus_o_viana@hotmail.com>
2020-01-03 16:42:45 -0300

    chore: added declarations into photos module and added FilterByDescriptionPipe into photo-list.module

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -2,8 +2,14 @@ import { NgModule } from '@angular/core';
 import { PhotoListComponent } from './photo-list.component';
 import { PhotosComponent } from './photos/photos.component';
 import { LoadButtonComponent } from './load-button/load-button.component';
+import { FilterByDescriptionPipe } from './filterByDescription.pipe';
 
 @NgModule({
-  declarations: [PhotoListComponent, PhotosComponent, LoadButtonComponent]
+  declarations: [
+    PhotoListComponent,
+    PhotosComponent,
+    LoadButtonComponent,
+    FilterByDescriptionPipe
+  ]
 })
 export class PhotoListModule {}
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -2,12 +2,8 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 
-import { PhotoComponent } from './photo/photo.component';
-import { PhotoListComponent } from './photo-list/photo-list.component';
-import { PhotoFormComponent } from './photo-form/photo-form.component';
-import { PhotosComponent } from './photo-list/photos/photos.component';
-import { FilterByDescriptionPipe } from './photo-list/filterByDescription.pipe';
-import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
+import { PhotoListModule } from './photo-list/photo-list.module';
+import { PhotoFormModule } from './photo-form/photo-form.module';
 
 /*
   A module can have lots ad lots of declarions, but I have to export those declarions to be accessible outside of this module
@@ -15,14 +11,7 @@ import { LoadButtonComponent } from './photo-list/load-button/load-button.compon
 // if I have 100 components in declarions I must have them into exports as well
 
 @NgModule({
-  declarations: [
-    PhotoComponent,
-    PhotoListComponent,
-    PhotoFormComponent,
-    PhotosComponent,
-    FilterByDescriptionPipe,
-    LoadButtonComponent
-  ],
-  imports: [HttpClientModule, CommonModule]
+  declarations: [],
+  imports: [HttpClientModule, CommonModule, PhotoListModule, PhotoFormModule]
 })
 export class PhotosModule {}
