b2fee5b87440c00a4277a1872050b4627b084f8b
Matheus Viana <matheus_o_viana@hotmail.com>
2020-01-06 17:37:51 -0300

    chore: added search component

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -7,13 +7,15 @@ import { LoadButtonComponent } from './load-button/load-button.component';
 import { FilterByDescriptionPipe } from './filterByDescription.pipe';
 import { PhotoModule } from '../photo/photo.module';
 import { CardModule } from '../../shared/components/card/card/card.module';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     PhotoListComponent,
     PhotosComponent,
     LoadButtonComponent,
-    FilterByDescriptionPipe
+    FilterByDescriptionPipe,
+    SearchComponent
   ],
   imports: [CommonModule, PhotoModule, CardModule]
 })
