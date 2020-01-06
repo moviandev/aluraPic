ab6c23735df8b29530b611fbd80c2b4d8b8f02b4
Matheus Viana <matheus_o_viana@hotmail.com>
2020-01-06 18:27:51 -0300

    chore: added ElementRef into our directive's constructor

--- src/app/shared/directives/darken-on-hover/darken-on-hover.directive.ts
+++ src/app/shared/directives/darken-on-hover/darken-on-hover.directive.ts
@@ -1,9 +1,9 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
 
 // to use <a apDarkenOnHover> is necessary to put the selector name into brackets
 @Directive({
   selector: '[apDarkenOnHover]'
 })
 export class DarkenOnHoverDirective {
-  constructor() {}
+  constructor(private el: ElementRef) {}
 }
