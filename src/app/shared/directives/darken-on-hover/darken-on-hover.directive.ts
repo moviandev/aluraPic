01a5b350d80083820d037b836a52f78370b9236f
Matheus Viana <matheus_o_viana@hotmail.com>
2020-01-06 18:50:57 -0300

    chore: added params into directive like components

--- src/app/shared/directives/darken-on-hover/darken-on-hover.directive.ts
+++ src/app/shared/directives/darken-on-hover/darken-on-hover.directive.ts
@@ -1,15 +1,27 @@
-import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  Renderer2,
+  Input
+} from '@angular/core';
 
 // to use <a apDarkenOnHover> is necessary to put the selector name into brackets
 @Directive({
   selector: '[apDarkenOnHover]'
 })
 export class DarkenOnHoverDirective {
+  @Input() brightness = '70%';
+
   constructor(private el: ElementRef, private render: Renderer2) {}
 
   @HostListener('mouseover')
   darkenOn() {
-    this.render.setStyle(this.el.nativeElement, 'filter', 'brightness(70%)');
+    this.render.setStyle(
+      this.el.nativeElement,
+      'filter',
+      `brightness(${this.brightness})`
+    );
   }
 
   @HostListener('mouseleave')
