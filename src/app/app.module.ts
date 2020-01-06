ab5fc026fe9a1fd3e4a6fb01f1ef052e922403c4
Matheus Viana <matheus_o_viana@hotmail.com>
2020-01-06 11:55:34 -0300

    chore: added shared folder and added card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app.routing.module';
 import { AppComponent } from './app.component';
 import { PhotosModule } from './photos/photos.module';
 import { ErrorsModule } from './errors/errors.module';
+import { CardComponent } from './shared/components/card/card/card.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CardComponent],
   imports: [BrowserModule, AppRoutingModule, PhotosModule, ErrorsModule],
   providers: [],
   bootstrap: [AppComponent]
