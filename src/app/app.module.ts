0b8b76cb1400f7c5a9c46e5a1e5e8616c1814ad2
Matheus Viana <matheus_o_viana@hotmail.com>
2020-01-06 19:01:49 -0300

    removed: deleted sharedModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,10 @@ import { AppRoutingModule } from './app.routing.module';
 import { AppComponent } from './app.component';
 import { PhotosModule } from './photos/photos.module';
 import { ErrorsModule } from './errors/errors.module';
-import { CardModule } from './shared/components/card/card/card.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    PhotosModule,
-    ErrorsModule,
-    CardModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, PhotosModule, ErrorsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
