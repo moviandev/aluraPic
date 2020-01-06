e1ba872b0506ba088d682c93de81cf01a8a65d64
Matheus Viana <matheus_o_viana@hotmail.com>
2020-01-06 12:00:55 -0300

    removed: deleted card component importation of app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,9 @@ import { AppRoutingModule } from './app.routing.module';
 import { AppComponent } from './app.component';
 import { PhotosModule } from './photos/photos.module';
 import { ErrorsModule } from './errors/errors.module';
-import { CardComponent } from './shared/components/card/card/card.component';
 
 @NgModule({
-  declarations: [AppComponent, CardComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, PhotosModule, ErrorsModule],
   providers: [],
   bootstrap: [AppComponent]
