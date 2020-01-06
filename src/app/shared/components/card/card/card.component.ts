11313af0675e1b6eb103d0e44cc99144710c4a00
Matheus Viana <matheus_o_viana@hotmail.com>
2020-01-06 11:59:13 -0300

    chore: added input property into card component and added template

--- src/app/shared/components/card/card/card.component.ts
+++ src/app/shared/components/card/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'ap-card',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
+  @Input() title: string;
+
   constructor() {}
 
   ngOnInit() {}
