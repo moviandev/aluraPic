6f8d24e9965fa99fbe1882bd25f35c343371086d
Matheus Viana <matheus_o_viana@hotmail.com>
2020-01-06 18:58:34 -0300

    chore: added shared module

--- src/app/shared/components/card/card/card.component.ts
+++ src/app/shared/components/card/card/card.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
-  @Input() title = '';
+  @Input() title: string;
 
   constructor() {}
 
