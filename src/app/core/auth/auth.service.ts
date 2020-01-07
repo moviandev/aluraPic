4e1acc55fabbdff4e7762ceea52c3635156d91ab
Matheus Viana <matheus_o_viana@hotmail.com>
2020-01-07 19:01:52 -0300

    chore: getting response headers

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
 
 const API = 'http://localhost:3000';
 @Injectable({
@@ -9,6 +10,16 @@ export class AuthService {
   constructor(private http: HttpClient) {}
 
   authenticate(username: string, password: string) {
-    return this.http.post(`${API}/user/login`, { username, password });
+    return this.http
+      .post(
+        `${API}/user/login`,
+        { username, password },
+        { observe: 'response' }
+      )
+      .pipe(
+        tap(res => {
+          const authToken = res.headers.get('x-access-token');
+        })
+      );
   }
 }
