e98ca6affbf1bea59cd6b83850e6604e9de7047b
Matheus Viana <matheus_o_viana@hotmail.com>
2020-01-07 13:36:48 -0300

    chore: coding refactoring

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { SigninModule } from './signin/signin.module';
-import { SignupModule } from './signup/signup.module';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, SigninModule, SignupModule]
+  declarations: [SigninComponent, SignupComponent],
+  imports: [CommonModule, ReactiveFormsModule]
 })
 export class HomeModule {}
