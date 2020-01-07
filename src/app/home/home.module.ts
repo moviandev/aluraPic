bd0e415f14d0bab2c86164c406e3e098b72f21da
Matheus Viana <matheus_o_viana@hotmail.com>
2020-01-07 11:08:22 -0300

    chore: imported Signin and Signup modules into home module

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { SigninModule } from './signin/signin.module';
+import { SignupModule } from './signup/signup.module';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, SigninModule, SignupModule]
 })
-export class HomeModule { }
+export class HomeModule {}
