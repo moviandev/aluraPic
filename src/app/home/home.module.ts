f5f27ef3f29af927a8910c5451177639facfcbd5
Matheus Viana <matheus_o_viana@hotmail.com>
2020-01-07 13:38:23 -0300

    chore: imported VmessageModule into home module

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,9 +4,10 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
+import { VmessageModule } from '../shared/components/vmessage/vmessage.module';
 
 @NgModule({
   declarations: [SigninComponent, SignupComponent],
-  imports: [CommonModule, ReactiveFormsModule]
+  imports: [CommonModule, ReactiveFormsModule, VmessageModule]
 })
 export class HomeModule {}
