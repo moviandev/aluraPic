ab3b8db82b50ee20419360cb0b42ba543903b841
Matheus Viana <matheus_o_viana@hotmail.com>
2020-01-07 11:05:37 -0300

    chore added signin component and added signup module and component

--- src/app/home/signup/signup.module.ts
+++ src/app/home/signup/signup.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
 
 
 
 @NgModule({
-  declarations: [SigninComponent],
+  declarations: [SignupComponent],
   imports: [
     CommonModule
   ]
 })
-export class SigninModule { }
+export class SignupModule { }
