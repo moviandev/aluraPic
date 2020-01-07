c56400b037d67fdd0ee68d9abb8174fe02127eaa
Matheus Viana <matheus_o_viana@hotmail.com>
2020-01-07 11:18:24 -0300

    chore: added routing to login and imported home module into app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,17 @@ import { AppRoutingModule } from './app.routing.module';
 import { AppComponent } from './app.component';
 import { PhotosModule } from './photos/photos.module';
 import { ErrorsModule } from './errors/errors.module';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, PhotosModule, ErrorsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    PhotosModule,
+    ErrorsModule,
+    HomeModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
