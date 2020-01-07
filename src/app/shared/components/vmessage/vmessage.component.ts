b024299c835df901294064cd466670003efb8353
Matheus Viana <matheus_o_viana@hotmail.com>
2020-01-07 13:31:40 -0300

    chore: added inboundo property into vmessage component

--- src/app/shared/components/vmessage/vmessage.component.ts
+++ src/app/shared/components/vmessage/vmessage.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-vmessage',
@@ -6,10 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vmessage.component.scss']
 })
 export class VmessageComponent implements OnInit {
+  @Input() text: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
