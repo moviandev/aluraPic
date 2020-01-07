3dfaa14acb8dd04b886d356dc92e88753faa3ca4
Matheus Viana <matheus_o_viana@hotmail.com>
2020-01-07 11:24:54 -0300

    chore: added formgroup

--- src/app/home/signin/signin/signin.component.ts
+++ src/app/home/signin/signin/signin.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.scss']
 })
 export class SigninComponent implements OnInit {
+  loginForm: FormGroup;
+
   constructor() {}
 
   ngOnInit() {}
